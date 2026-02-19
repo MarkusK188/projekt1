2d6332adabcb80a354bf166f3dcffe18e89605a0
Markus Karm <markus.karm@tpt.lan>
2026-02-19 12:07:03 +0200

    KanyeQuote Pokemon Update

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let quote = document.querySelector('#quote');
 let rndmButton = document.querySelector('#btn');
 
-
+let randomId = 0
 
 async function randomQuote() {
 	const response = await fetch('https://api.kanye.rest');
 	let data = await response.json();
 	console.log(data);
 	quote.textContent = data.quote;
+	randomPokemon();
+
 };
 
 btn.addEventListener("click", randomQuote)
 
 
+async function randomPokemon() {
+	let randomId = Math.floor(Math.random() * 151) +1;
 
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+	let data = await response.json();
+	const pokemonSprite = data.sprites.front_default;
+	const imgElement = document.getElementById('pokemonSprite');
+
+	imgElement.src = pokemonSprite;
+	imgElement.style.display = "block";
+}
